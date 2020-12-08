69d8fbdd8dc410423d29fce1185e984890f2b993
Ajith <eajithkumar12879@outlook.com>
2020-12-08 23:13:17 +0530

    adding logs

--- app.js
+++ app.js
@@ -17,50 +17,63 @@ app.get('/', (req, res) => {
 })
 
 app.get("/teams", async (req, res) => {
-    database.connect()
-    let response = await matchScore.find({}).exec()
-    res.send(response)
+    try {
+        database.connect()
+        let response = await matchScore.find({}).exec()
+        res.send(response)
+    } catch (e) {
+        res.status = 500
+        res.send({ message: "Error in getting the result" })
+    }
+
 })
 
 app.post("/matchResult", async (req, res) => {
-    database.connect()
-    if (req.body.hasOwnProperty('winner')) {
-        let winner = req.body.winner
-        let loser = req.body.loser
-        let updateWinner = await matchScore.findOne({ team_name: winner })
-        updateWinner.wins = updateWinner.wins + 1
-        updateWinner.score = updateWinner.score + 3
-        updateWinner.save()
-        let updateLoser = await matchScore.findOne({ team_name: loser })
-        updateLoser.losses = updateWinner.losses + 1
-        updateLoser.save()
-    } else {
-        let ties = req.body.ties
-        try {
+    try {
+        database.connect()
+        if (req.body.hasOwnProperty('winner')) {
+            let winner = req.body.winner
+            let loser = req.body.loser
+            let updateWinner = await matchScore.findOne({ team_name: winner })
+            updateWinner.wins = updateWinner.wins + 1
+            updateWinner.score = updateWinner.score + 3
+            updateWinner.save()
+            let updateLoser = await matchScore.findOne({ team_name: loser })
+            updateLoser.losses = updateWinner.losses + 1
+            updateLoser.save()
+        } else {
+            let ties = req.body.ties
             let updateTies = await matchScore.updateMany({ team_name: { $in: ties } },
                 {
                     $inc: { score: 1, ties: 1 }
                 }
             );
-        } catch (e) {
-            console.log(e)
         }
-
+        let result = await matchScore.find({}).exec()
+        res.send(result)
+    } catch (e) {
+        res.status = 500;
+        res.send({ message: "Error in updating the result" })
     }
-    let result = await matchScore.find({}).exec()
-    res.send(result)
+
 })
 
 app.post("/addNewTeam", async (req, res) => {
-    let newUser = req.body
-    newUser.wins = 0
-    newUser.losses = 0
-    newUser.ties = 0
-    newUser.score = 0
-    database.connect()
-    await matchScore.insertMany([newUser]);
-    let result = await matchScore.find({}).exec()
-    res.send(result)
+    try {
+        let newUser = req.body
+        newUser.wins = 0
+        newUser.losses = 0
+        newUser.ties = 0
+        newUser.score = 0
+        database.connect()
+        await matchScore.insertMany([newUser]);
+        let result = await matchScore.find({}).exec()
+        res.send(result)
+    } catch (e) {
+        res.status = 500;
+        res.send({ message: "Team with same name alrady exist" })
+    }
+
 })
 
 app.listen(process.env.PORT, () => {
